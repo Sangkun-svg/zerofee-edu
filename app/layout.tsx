9654e29bd1a1d1e9056bd59d17b526184540d3f8
Sangkun <sangkun@nocoders.kr>
2026-04-15 19:15:58 +0900

    build: set metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,27 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "zerofee edu",
-  description: "교육 결제 플랫폼 zerofee edu",
+  title: "제로피 - 수수료 0% 판매 상점",
+  description:
+    "디지털 상품부터 링크페이지까지 수수료 0%, 판매 금액 그대로 크리에이터의 수익이 됩니다. 지금 제로피에서 무료 결제창을 만드세요.",
+  icons: {
+    icon: "/zerofee-favicon.png",
+  },
+  openGraph: {
+    title: "제로피 - 수수료 0% 판매 상점",
+    siteName: "제로피",
+    description:
+      "디지털 상품부터 링크페이지까지 수수료 0%, 판매 금액 그대로 크리에이터의 수익이 됩니다. 지금 제로피에서 무료 결제창을 만드세요.",
+    type: "website",
+    images: [{ url: "/zerofee-edu-og.png" }],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "제로피 - 수수료 0% 판매 상점",
+    description:
+      "디지털 상품부터 링크페이지까지 수수료 0%, 판매 금액 그대로 크리에이터의 수익이 됩니다. 지금 제로피에서 무료 결제창을 만드세요.",
+    images: ["/zerofee-edu-og.png"],
+  },
 };
 
 export default function RootLayout({
