1e830fc624596039cb4c73fa0f89368d97261275
Sangkun <sangkun@nocoders.kr>
2026-04-16 11:46:50 +0900

    build: set fav,og

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   description:
     "디지털 상품부터 링크페이지까지 수수료 0%, 판매 금액 그대로 크리에이터의 수익이 됩니다. 지금 제로피에서 무료 결제창을 만드세요.",
   icons: {
-    icon: "/zerofee-favicon.png",
+    icon: "/images/zerofee-favicon.png",
   },
   openGraph: {
     title: "제로피 - 수수료 0% 판매 상점",
@@ -14,14 +14,14 @@ export const metadata: Metadata = {
     description:
       "디지털 상품부터 링크페이지까지 수수료 0%, 판매 금액 그대로 크리에이터의 수익이 됩니다. 지금 제로피에서 무료 결제창을 만드세요.",
     type: "website",
-    images: [{ url: "/zerofee-edu-og.png" }],
+    images: [{ url: "/images/zerofee-edu-og.png" }],
   },
   twitter: {
     card: "summary_large_image",
     title: "제로피 - 수수료 0% 판매 상점",
     description:
       "디지털 상품부터 링크페이지까지 수수료 0%, 판매 금액 그대로 크리에이터의 수익이 됩니다. 지금 제로피에서 무료 결제창을 만드세요.",
-    images: ["/zerofee-edu-og.png"],
+    images: ["/images/zerofee-edu-og.png"],
   },
 };
 
